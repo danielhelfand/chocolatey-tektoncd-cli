aff1feb7cbf802c5c2b28201343e784aa382b4f2
Daniel Helfand <helfand.4@gmail.com>
2020-04-29 10:54:27 -0400

    update to 0.9.0

--- tektoncd-cli/tools/chocolateyinstall.ps1
+++ tektoncd-cli/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.8.0/tkn_0.8.0_Windows_x86_64.zip' 
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.9.0/tkn_0.9.0_Windows_x86_64.zip' 
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'DCEB9CA365BD167E547BF99CCD5BE6F24CE431D353CF6C4BCDD7E3847A6ACC42'
+  checksum64     = '95a957d511c1109b675f847e7d3d94a39a37a6cd3d43dc95cc662710907ba858'
   checksumType64 = 'sha256'
 }
 
