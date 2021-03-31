fc9b5dfbdec97e255e9cb65c298b05cbcc7ad14c
Daniel Helfand <helfand.4@gmail.com>
2021-03-31 10:06:08 -0400

    update tkn to v0.17.1

--- tektoncd-cli/tools/chocolateyinstall.ps1
+++ tektoncd-cli/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.17.0/tkn_0.17.0_Windows_x86_64.zip' 
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.17.1/tkn_0.17.1_Windows_x86_64.zip' 
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'f7b380d43499693137240883502cdf18bc42f6d15f5cc23991dac6488c8d93f1'
+  checksum64     = '1b4aec72752f9e20462374d314e48138fdab2b951500d24580f8c750233ae460'
   checksumType64 = 'sha256'
 }
 
